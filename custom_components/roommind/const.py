a2de521065e50d7c859438cd1d91f6b169bfe9f3
snazzybean <17204760+snazzybean@users.noreply.github.com>
2026-03-27 21:27:33 +0100

    chore: bump version to 1.6.1-beta.2

--- custom_components/roommind/const.py
+++ custom_components/roommind/const.py
@@ -7,7 +7,7 @@ from homeassistant.const import Platform
 from homeassistant.core import Context
 
 DOMAIN = "roommind"
-VERSION = "1.6.1-beta.1"
+VERSION = "1.6.1-beta.2"
 
 # Platforms
 PLATFORMS = [Platform.SENSOR, Platform.SWITCH, Platform.BINARY_SENSOR, Platform.CLIMATE]
